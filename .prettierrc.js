b9d9195ac1fa4adaac8317c9bb6b067323dff165
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2022-09-17 20:10:58 +0800

    init ci pipeline (#2)
    * init husky
    
    * setup ci pipeline
    
    * change package name

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.export = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQUote: true,
+  arrowParens: "always",
+};
