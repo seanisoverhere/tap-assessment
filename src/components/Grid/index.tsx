d749911b7063503ea242cfee0eecbe22ae72b904
seanisoverhere <sean_choon@hotmail.com>
2022-09-18 00:44:19 +0800

    add rotating background

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
+import { animate, useMotionValue } from 'framer-motion'
 import Cell from '@/components/Cell'
 import { CELL_SIZE } from '@/utils/constants'
 
 const Grid = () => {
   const [columns, setColumns] = useState<number>(0)
   const [rows, setRows] = useState<number>(0)
+
+  const mouseX = useMotionValue(0)
+  const mouseY = useMotionValue(0)
 
   useEffect(() => {
     const calculateGrid = () => {
@@ -22,13 +25,25 @@ const Grid = () => {
     return () => {
       window.removeEventListener('resize', calculateGrid)
     }
+  }, [])
 
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      animate(mouseX, e.clientX)
+      animate(mouseY, e.clientY)
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
   }, [])
 
   return (
     <div className='grid-container' style={{ gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))` }}>
       {Array.from({ length: columns * rows }).map((_, i) => (
-        <Cell key={i} />
+        <Cell key={i} mouseX={mouseX} mouseY={mouseY} />
       ))}
     </div>
   )
